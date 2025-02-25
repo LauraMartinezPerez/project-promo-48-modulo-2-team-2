4841fae126628920a1059003c825cdd9870fd1cd
Paloma Fernandez Jaen <palomafj91@gmail.com>
2025-02-25 15:10:35 +0100

    solucionado

--- src/js/collapsables.js
+++ src/js/collapsables.js
@@ -1,11 +1,8 @@
 console.log("fichero colapsables");
 
-
-
-
 const userName = document.querySelector(".js-name");
-const listName = document.querySelector("js-list-name");
-const img = document.querySelector("js-img");
+const listName = document.querySelector(".js-list-name");
+const img = document.querySelector(".js-img");
 
 const designTitle = document.querySelector(".js-design-title");
 const designForm = document.querySelector(".js-design-form");
@@ -13,7 +10,7 @@ const designForm = document.querySelector(".js-design-form");
 function showAndHiddenForm() {    
     designForm.classList.toggle("collapsed");
     console.log("escuchando el click");
-    if (designTitle.classList.contains("collapsed"))
+    if (designTitle.classList.contains("collapsed")){}
 }
 
 designTitle.addEventListener("click", (event) => {
