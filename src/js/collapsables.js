562e9989b0b25a459a97a5f9429500a84b671f1a
Laura <laura.laukiz@gmail.com>
2025-02-25 16:43:58 +0100

    collapsed Lau

--- src/js/collapsables.js
+++ src/js/collapsables.js
@@ -1,22 +1,24 @@
-console.log("fichero colapsables");
+/* console.log("fichero colapsables");
 
 
 
 
 const userName = document.querySelector(".js-name");
-const listName = document.querySelector("js-list-name");
-const img = document.querySelector("js-img");
+const listName = document.querySelector(".js-list-name");
+const img = document.querySelector(".js-img");
 
 const designTitle = document.querySelector(".js-design-title");
 const designForm = document.querySelector(".js-design-form");
 
-function showAndHiddenForm() {    
-    designForm.classList.toggle("collapsed");
-    console.log("escuchando el click");
-    if (designTitle.classList.contains("collapsed"))
-}
+function handleClickFormDesign () {
+    if (designForm.classList.contains ("collapsed")) {
+        designForm.classList.remove ("collapsed");
+    }
+    else {
+        designForm.classList.add ("collapsed");
+    }
+}  
 
-designTitle.addEventListener("click", (event) => {
-    event.preventDefault();
-    showAndHiddenForm();
-})
+designTitle.addEventListener ("click", handleClickFormDesign);
+
+ */
