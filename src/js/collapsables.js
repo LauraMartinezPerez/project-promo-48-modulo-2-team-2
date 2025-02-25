80842976eba59fc6542aff4ee4122c452c0a8d79
Judit Casamitjana <juditcasamitjana@gmail.com>
2025-02-25 13:23:37 +0100

    add js

--- src/js/collapsables.js
+++ src/js/collapsables.js
@@ -6,3 +6,17 @@ console.log("fichero colapsables");
 const userName = document.querySelector(".js-name");
 const listName = document.querySelector("js-list-name");
 const img = document.querySelector("js-img");
+
+const designTitle = document.querySelector(".js-design-title");
+const designForm = document.querySelector(".js-design-form");
+
+function showAndHiddenForm() {    
+    designForm.classList.toggle("collapsed");
+    console.log("escuchando el click");
+    if (designTitle.classList.contains("collapsed"))
+}
+
+designTitle.addEventListener("click", (event) => {
+    event.preventDefault();
+    showAndHiddenForm();
+})
