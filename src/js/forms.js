138cbd642b9c083eb089a2a846296b6fdca0346a
MafeToro <toro.maria.fernanda@gmail.com>
2025-02-26 19:40:43 +0100

    prev function

--- src/js/forms.js
+++ src/js/forms.js
@@ -1,2 +1,11 @@
 console.log("ficheros forms");
 
+const userNamelist = document.querySelector(".js-name");
+const userListName = document.querySelector(".js-list-name");
+
+/*Faltan las constantes donde se pintan estos dos datos 
+en la previsualización
+userListName.addEventListener("input", (event) => {
+nombre de dónde se rellena la tarjeta al previsualizar.innerHTML = event.target.value;
+
+})*/
