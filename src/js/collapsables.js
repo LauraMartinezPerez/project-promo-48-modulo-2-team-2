a5ccabd945eca753935133d7d7d034f3f3297da1
Laura <laura.laukiz@gmail.com>
2025-03-07 12:44:16 +0100

    optimizando collapsables con bucles

--- src/js/collapsables.js
+++ src/js/collapsables.js
@@ -1,4 +1,4 @@
-console.log("fichero colapsables");
+
 
 const userName = document.querySelector(".js-name");
 const listName = document.querySelector(".js-list-name");
@@ -10,8 +10,7 @@ const designForm = document.querySelector(".js-design-form");
 const fillTitle = document.querySelector(".js-fill-title");
 const fillForm = document.querySelector(".js-fill-form");
 const shareTitle = document.querySelector(".js-share-title");
-const shareForm = document.querySelector(".js-share-form");
-
+const shareForm = document.querySelector(".js-share-form"); 
 
 //Mostrar form design
 function showFormDesign () {
@@ -23,7 +22,53 @@ function showFormDesign () {
 function hideFormDesign () {
     designForm.classList.add("collapsed");
 }
-//Alternar abrir y cerrar form design
+//Mostrar form fill
+function showFormFill () {
+    hideFormDesign ();
+    hideFormShare ();
+    fillForm.classList.remove("collapsed");
+}
+//Ocultar form fill
+function hideFormFill () {
+    fillForm.classList.add("collapsed");
+}
+//Mostrar form share
+function showFormShare () {
+    hideFormDesign ();
+    hideFormFill();
+    shareForm.classList.remove("collapsed");
+}
+//Ocultar form share
+function hideFormShare () {
+    shareForm.classList.add("collapsed");
+}
+
+const collapsableHeaders = document.querySelectorAll(".js-design-header");
+
+const handleClick = (event) => {
+    if (event.currentTarget.classList.contains("js-design-title")) {
+        showFormDesign ()
+    } else if (event.currentTarget.classList.contains("js-fill-title")) {
+        showFormFill ()
+    } else {showFormShare ()}
+}
+for (const title of collapsableHeaders) {
+    title.addEventListener("click", handleClick);
+}
+
+
+/* 
+//Mostrar form design
+function showFormDesign () {
+    hideFormFill();
+    hideFormShare();
+    designForm.classList.remove("collapsed");
+}
+//Ocultar form Design
+function hideFormDesign () {
+    designForm.classList.add("collapsed");
+}
+ //Alternar abrir y cerrar form design
 function showAndHideFormDesign () {
     if (designForm.classList.contains("collapsed")) {
         showFormDesign ();
@@ -45,7 +90,7 @@ function showFormFill () {
 function hideFormFill () {
     fillForm.classList.add("collapsed");
 }
-//Alternar abrir y cerrar form fill
+ //Alternar abrir y cerrar form fill
 function showAndHideFormFill () {
     if (fillForm.classList.contains("collapsed")) {
         showFormFill ();
@@ -56,7 +101,7 @@ function showAndHideFormFill () {
 }
 
 fillTitle.addEventListener ("click", showAndHideFormFill);
-
+ 
 //Mostrar form share
 function showFormShare () {
     hideFormDesign ();
@@ -67,7 +112,7 @@ function showFormShare () {
 function hideFormShare () {
     shareForm.classList.add("collapsed");
 }
-//Alternar abrir y cerrar form share
+ //Alternar abrir y cerrar form share
 function showAndHideFormShare () {
     if (shareForm.classList.contains("collapsed")) {
         showFormShare ();
@@ -77,6 +122,5 @@ function showAndHideFormShare () {
     }
 }
 
-shareTitle.addEventListener ("click", showAndHideFormShare);
-
-
+shareTitle.addEventListener ("click", showAndHideFormShare);  */
+  
