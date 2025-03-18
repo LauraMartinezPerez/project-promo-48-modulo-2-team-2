d36ca9649b658cbe6fbc484d98aba610f5707f67
MafeToro <toro.maria.fernanda@gmail.com>
2025-03-18 18:58:06 +0100

    try

--- src/js/searchcard.js
+++ src/js/searchcard.js
@@ -5,15 +5,15 @@ const linkCard = document.querySelector(".js-link-created");
 
 function handleCreateCard(ev) {
     ev.preventDefault();
-
+    // Función para pintar nombre de usuario y nombre de lista //
+//endpoint de tipo POST
     fetch("https://dev.adalab.es/api/info/data", {
-        method: "POST",
-        body: JSON.stringify(FormData),
+        method: "POST", // si quiero que sea POST, es obligatorio, por default es GET
+        body: JSON.stringify(formData), // Los datos que quiero recogger
         headers: { "Content-type": "application/json" },
     })
         .then((response) => response.json())
         .then((data) => {
-            console.log(data);
             const idCard = data.infoID;
             linkCard.classList.remove("hidden");
             linkCard.href = `./card.html?id=${idCard}`;
