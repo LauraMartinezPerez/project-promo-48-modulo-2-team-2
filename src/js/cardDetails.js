1b0776cb58585ad34897d2dcf2c9511169ed664c
MafeToro <toro.maria.fernanda@gmail.com>
2025-03-19 21:47:46 +0100

    solving problems

--- src/js/cardDetails.js
+++ src/js/cardDetails.js
@@ -16,6 +16,14 @@ const movieTwoCard = document.querySelector(".js-peli2");
 const movieThreeCard = document.querySelector(".js-peli3");
 const userPhotoCard = document.querySelector(".js-user-photo");
 
+//Para coger la info de la url de una pagina:
+
+//me creo una variable:
+const ulrParam = new URLSearchParams(window.location.search);
+//ahora me cojo el id:
+const id = ulrParam.get("id");
+console.log("ID obtenido:", id);
+
 //Para pintar el genero correctamente en la card.
 const getGenre = (genreId) => {
     switch (genreId) {
@@ -39,16 +47,10 @@ const getBackground = (genreId) => {
     }
 };
 
-//Para coger la info de la url de una pagina:
-
-//me creo una variable:
-const ulrParam = new URLSearchParams(window.location.search);
-//ahora me cojo el id:
-const id = ulrParam.get("id");
 
 fetch(`https://dev.adalab.es/api/info/${id}`)
-    .then((response) => response.json())
-    .then((data) => {
+    .then(response => response.json())
+    .then(data => {
         console.log(data);
         const targetData = data.data;
         titleCard.innerHTML = targetData.field3;
@@ -59,5 +61,7 @@ fetch(`https://dev.adalab.es/api/info/${id}`)
         movieTwoCard.innerHTML = targetData.field5;
         movieThreeCard.innerHTML = targetData.field6;
         userPhotoCard.src = targetData.photo;
-        localStorage.setItem("targetData", JSON.stringify(targetData));
-    });
+    })
+
+    localStorage.setItem(targetData, "targetData")
+
--- src/js/searchcard.js
+++ src/js/searchcard.js
@@ -4,22 +4,24 @@ const createCardBtn = document.querySelector(".js-btn-create");
 const linkCard = document.querySelector(".js-link-created");
 const linkWhatsapp = document.querySelector(".js-link-whatsapp");
 
-function handleCreateCard(ev) {
+function handleCreateCard (ev) {
     ev.preventDefault();
     // Función para pintar nombre de usuario y nombre de lista //
 //endpoint de tipo POST
     fetch("https://dev.adalab.es/api/info/data", {
         method: "POST", // si quiero que sea POST, es obligatorio, por default es GET
         body: JSON.stringify(FormData), // Los datos que quiero recogger
-        headers: { "Content-type": "application/json" },
+        headers: { "Content-type": "application/json" }
     })
-        .then((response) => response.json())
-        .then((data) => {
+        .then(response => response.json())
+        .then(data => {
             const idCard = data.infoID;
             linkCard.classList.remove("hidden");
-            linkCard.href = `./card.html?id=${idCard}`;
-            linkWhatsapp.href += encodeURI(linkCard.href);
-        });
+            linkCard.href = `./card.html?id=${idCard}`
+            linkWhatsapp.href += encodeURI(linkCard.href)
+        })
 }
 
 createCardBtn.addEventListener("click", handleCreateCard);
+
+
